1a18f8dae4e0df3a23e30adfdaad4c66b446a20a
home <andrijkutovij5@gmail.com>
2023-12-05 12:31:41 +0200

    -redux

--- src/components/TodosComponent/Todo/Todo.js
+++ src/components/TodosComponent/Todo/Todo.js
@@ -1,13 +1,37 @@
-import React from 'react';
-import {useSelector} from "react-redux";
-import style from "../TodosComponent.module.css";
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import style from '../TodosComponent.module.css';
 
 const Todo = () => {
-    const todos = useSelector((state) => state.todos.todos)
+    const todos = useSelector((state) => state.todos.todos);
+
+    const [selectedItems, setSelectedItems] = useState([]);
+
+    const handleItemClick = (index) => {
+        setSelectedItems((prevSelected) => {
+            if (prevSelected.includes(index)) {
+                // Если элемент уже был выбран, убираем его из массива выделенных
+                return prevSelected.filter((item) => item !== index);
+            } else {
+                // Иначе добавляем элемент в массив выделенных
+                return [...prevSelected, index];
+            }
+        });
+    };
 
     return (
         <div className={style.list}>
-            {todos.map((todo) => todo )}
+            {todos.map((todo, index) => (
+                <div
+                    key={index}
+                    onClick={() => handleItemClick(index)}
+                    style={{
+                        backgroundColor: selectedItems.includes(index) ? 'yellow' : 'white',
+                    }}
+                >
+                    {todo}
+                </div>
+            ))}
         </div>
     );
 };
